824e2c97094dd9ef5e73e33881188a548a62ee8e
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-07-04 11:59:25 +0530

    Added a welcome message
     Changes to be committed:
    	modified:   src/main/java/io/github/boogiemonster1o1/armorhud/ArmorHud.java

--- src/main/java/io/github/boogiemonster1o1/armorhud/ArmorHud.java
+++ src/main/java/io/github/boogiemonster1o1/armorhud/ArmorHud.java
@@ -1,10 +1,11 @@
 package io.github.boogiemonster1o1.armorhud;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
 
 public class ArmorHud implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        LogManager.getLogger(ArmorHud.class).info("[ArmorHud] Initializing");
     }
 }
