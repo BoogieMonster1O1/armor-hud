f78b382fa456bc5a9d4a66e2a0fd5c1e0934971c
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-07-04 11:56:43 +0530

    Mod-ified files
     Changes to be committed:
    	modified:   .gitignore
    	new file:   src/main/java/io/github/boogiemonster1o1/armorhud/ArmorHud.java
    	deleted:    src/main/java/net/fabricmc/example/ExampleMod.java
    	deleted:    src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
    	renamed:    src/main/resources/modid.mixins.json -> src/main/resources/armorhud.mixins.json
    	modified:   src/main/resources/fabric.mod.json

---
+++ src/main/java/io/github/boogiemonster1o1/armorhud/ArmorHud.java
@@ -0,0 +1,10 @@
+package io.github.boogiemonster1o1.armorhud;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ArmorHud implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
